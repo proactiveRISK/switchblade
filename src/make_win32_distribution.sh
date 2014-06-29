2dc9325a14c2dbbd675186bd975c94d0b77a9d80
sammydre <sam.jansen@gmail.com>
2014-06-29 16:07:41 +0100

    Fix release script, update gitignore

--- src/make_win32_distribution.sh
+++ src/make_win32_distribution.sh
@@ -9,6 +9,12 @@ cp ../build/src/http_dos_cli.exe .
 python setup.py py2exe
 
 gtk_dir="/c/Program Files/Gtk+"
+gtk_locale_dir="/c/Program Files/Gtk+/share/locale"
+
+if [ ! -d "${gtk_dir}" ]; then
+  gtk_dir="/c/Program Files/GIMP 2/32"
+  gtk_locale_dir="/c/Program Files/GIMP 2/share/locale"
+fi
 
 # py2exe misses intl.dll
 cp "${gtk_dir}/bin/intl.dll" dist/
@@ -17,8 +23,8 @@ cp "${gtk_dir}/bin/intl.dll" dist/
 mkdir -p "dist/share/themes/Default" "dist/share/locale"
 #cp -r "${gtk_dir}/share/themes/Default" dist/share/themes
 cp -r "${gtk_dir}/share/themes/MS-Windows/" dist/share/themes/Default
-cp -r "${gtk_dir}/share/locale/en_GB"   dist/share/locale
-cp -r "${gtk_dir}/share/locale/en_CA"   dist/share/locale
+cp -r "${gtk_locale_dir}/en_GB"   dist/share/locale
+cp -r "${gtk_locale_dir}/en_CA"   dist/share/locale
 
 VERSION=$(cat VERSION)
 
